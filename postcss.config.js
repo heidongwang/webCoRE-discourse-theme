22f652e9e1fbbdeca7bd755a5365a96fb163b43a
ipaterson <ian@ipaterson.com>
2017-12-31 11:48:14 -0500

    Use stylelint for SCSS consistency

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,22 @@
+const ignoredWarnings = /Cannot find module '(theme_variables)'/;
+
 module.exports = {
 	parser: 'postcss-scss',
 	plugins: [
+		require('stylelint'),
 		require('postcss-easy-import')({
 			prefix: '_',
 			extensions: ['.scss'],
 			path: 'src/stylesheets',
+			plugins: [
+				require('stylelint'),
+			],
 		}),
 		require('postcss-optional-comments'),
-		require('autoprefixer')({
-			flexbox: 'no-2009',
+		require('autoprefixer'),
+		require('postcss-reporter')({
+			clearAllMessages: true,
+			filter: message => message.type === 'warning' && message.text && !ignoredWarnings.test(message.text),
 		}),
 	],
 };
